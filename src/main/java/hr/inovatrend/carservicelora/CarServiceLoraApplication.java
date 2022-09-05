548cf6edcaac85ed58bb1b81be58b611d2f43983
M. David <david.majdandzic@inovatrend.com>
2022-09-05 09:22:11 +0200

    Initial commit

---
+++ src/main/java/hr/inovatrend/carservicelora/CarServiceLoraApplication.java
@@ -0,0 +1,13 @@
+package hr.inovatrend.carservicelora;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarServiceLoraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarServiceLoraApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hr/inovatrend/carservicelora/CarServiceLoraApplicationTests.java
@@ -0,0 +1,13 @@
+package hr.inovatrend.carservicelora;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarServiceLoraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
