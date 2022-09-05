9537b797e22e1228318b73d3e84ae771cd86de24
M. David <david.majdandzic@inovatrend.com>
2022-09-05 10:48:04 +0200

    Update

--- src/main/java/hr/inovatrend/carservicelora/repository/UserRepository.java
+++ src/main/java/hr/inovatrend/carservicelora/repository/UserRepository.java
@@ -4,4 +4,5 @@ import hr.inovatrend.carservicelora.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+	void deleteById(Long id);
 }
