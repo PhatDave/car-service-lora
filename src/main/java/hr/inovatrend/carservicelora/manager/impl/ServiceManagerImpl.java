9dcc9791590fc58d8dfce9eccaf16fbbe7e4463d
lora.duvnjak <lora.duvnjak@gmail.com>
2022-09-21 11:04:37 +0200

    Added create service implementation

--- src/main/java/hr/inovatrend/carservicelora/manager/impl/ServiceManagerImpl.java
+++ src/main/java/hr/inovatrend/carservicelora/manager/impl/ServiceManagerImpl.java
@@ -3,16 +3,21 @@ package hr.inovatrend.carservicelora.manager.impl;
 import hr.inovatrend.carservicelora.entity.Car;
 import hr.inovatrend.carservicelora.entity.Service;
 import hr.inovatrend.carservicelora.manager.ServiceManager;
+import hr.inovatrend.carservicelora.repository.ServiceRepository;
+import lombok.RequiredArgsConstructor;
 
 import java.util.List;
 import java.util.Optional;
 
+@org.springframework.stereotype.Service
+@RequiredArgsConstructor
 public class ServiceManagerImpl implements ServiceManager {
 
+    private final ServiceRepository serviceRepository;
 
     @Override
     public Service createService(Service service) {
-        return null;
+        return serviceRepository.save(service);
     }
 
     @Override
