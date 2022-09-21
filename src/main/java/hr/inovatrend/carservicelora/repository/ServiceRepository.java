5c8769a96550fc2c1f361c1eb50b37e25737ad07
lora.duvnjak <lora.duvnjak@gmail.com>
2022-09-21 10:59:00 +0200

    Added service repository

---
+++ src/main/java/hr/inovatrend/carservicelora/repository/ServiceRepository.java
@@ -0,0 +1,9 @@
+package hr.inovatrend.carservicelora.repository;
+
+import hr.inovatrend.carservicelora.entity.Service;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ServiceRepository extends JpaRepository<Service, Long> {
+
+
+}
